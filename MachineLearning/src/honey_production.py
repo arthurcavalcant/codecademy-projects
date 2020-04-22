eecfd9f64c38a1c837334a0f6adfbd0baf6e4925
Arthur <arthur.ferreira@ccc.ufcg.edu.br>
2020-04-22 16:09:08 -0300

    Add Honey Production project

---
+++ MachineLearning/src/honey_production.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+df = pd.read_csv("https://s3.amazonaws.com/codecademy-content/programs/data-science-path/linear_regression/honeyproduction.csv")
+# print(df.head())
+
+prod_per_year = df.groupby("year").totalprod.mean().reset_index()
+# print(prod_per_year)
+
+X = prod_per_year["year"]
+X = X.values.reshape(-1, 1)
+# print(X)
+
+y = prod_per_year["totalprod"]
+# print(y)
+
+plt.scatter(X, y)
+# plt.show()
+
+regr = linear_model.LinearRegression()
+
+regr.fit(X, y)
+# print(regr.coef_)
+# print(regr.intercept_)
+
+y_predict = regr.predict(X)
+plt.plot(X, y_predict)
+# plt.show()
+
+X_future = np.array(range(2013, 2051))
+# print(X_future)
+X_future = X_future.reshape(-1, 1)
+# print(X_future)
+
+future_predict = regr.predict(X_future)
+plt.plot(X_future, future_predict)
+plt.show()
+
